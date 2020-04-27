865e6ca94d4f32f441ef99b1265f3b47814c0de1
kokaterimon <bpaucarm@outlook.com>
2020-04-26 22:23:47 -0500

    Estando el tutorial 13

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -22,6 +22,16 @@ public class EnemyController : MonoBehaviour{
         float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
         rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
 
-        //if
+        if (rb2d.velocity.x > -0.01f && rb2d.velocity.x < 0.01f){
+            speed = -speed;
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+        }
+        if (speed < 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }else if (speed > -0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
     }
 }
